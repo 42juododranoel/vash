1371b87ae391a32e944e5501d5256cb25ed2177c
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2019-05-27 13:55:10 +0300

    Improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export PROJECT_NAME='vash'
+export SSHPASS=${PRODUCTION_SERVER_PASSWORD}
 
 
 # Production directory structure
@@ -33,36 +34,40 @@ export PRODUCTION_ENVIRONMENT_ROOT='~/environment'
 
 # Functions
 
-function create_directory_structure {
-  mkdir -p ${PRODUCTION_ENVIRONMENT_ROOT}
+function create_directory_structure_on_production {
+  run_on_production "
+    mkdir -p ${PRODUCTION_ENVIRONMENT_ROOT}
 
-  mkdir -p ${PRODUCTION_ARTEFACTS_ROOT} \
-           ${PRODUCTION_ARTEFACTS_DIRECTORY} \
-           ${PRODUCTION_ARTEFACTS_STATIC_DIRECTORY} \
-           ${PRODUCTION_ARTEFACTS_POSTGRES_DIRECTORY}
+    mkdir -p ${PRODUCTION_ARTEFACTS_ROOT} \
+             ${PRODUCTION_ARTEFACTS_DIRECTORY} \
+             ${PRODUCTION_ARTEFACTS_STATIC_DIRECTORY} \
+             ${PRODUCTION_ARTEFACTS_POSTGRES_DIRECTORY}
 
-  mkdir -p ${PRODUCTION_BACKUPS_ROOT} \
-           ${PRODUCTION_BACKUPS_DIRECTORY} \
-           ${PRODUCTION_BACKUPS_POSTGRES_DIRECTORY} \
+    mkdir -p ${PRODUCTION_BACKUPS_ROOT} \
+             ${PRODUCTION_BACKUPS_DIRECTORY} \
+             ${PRODUCTION_BACKUPS_POSTGRES_DIRECTORY} \
 
-  mkdir -p ${PRODUCTION_IMAGES_ROOT} \
-           ${PRODUCTION_IMAGES_DIRECTORY} \
-           ${PRODUCTION_IMAGES_NGINX_DIRECTORY} \
-           ${PRODUCTION_IMAGES_DJANGO_DIRECTORY} \
-           ${PRODUCTION_IMAGES_POSTGRES_DIRECTORY}
+    mkdir -p ${PRODUCTION_IMAGES_ROOT} \
+             ${PRODUCTION_IMAGES_DIRECTORY} \
+             ${PRODUCTION_IMAGES_NGINX_DIRECTORY} \
+             ${PRODUCTION_IMAGES_DJANGO_DIRECTORY} \
+             ${PRODUCTION_IMAGES_POSTGRES_DIRECTORY}
 
-  mkdir -p ${PRODUCTION_REPOSITORIES_ROOT} \
-           ${PRODUCTION_REPOSITORIES_DIRECTORY}
+    mkdir -p ${PRODUCTION_REPOSITORIES_ROOT} \
+             ${PRODUCTION_REPOSITORIES_DIRECTORY}
 
-  mkdir -p ${PRODUCTION_RESOURCES_ROOT} \
-           ${PRODUCTION_RESOURCES_DIRECTORY} \
-           ${PRODUCTION_RESOURCES_MEDIA_DIRECTORY} \
-           ${PRODUCTION_RESOURCES_CERTIFICATES_DIRECTORY}
+    mkdir -p ${PRODUCTION_RESOURCES_ROOT} \
+             ${PRODUCTION_RESOURCES_DIRECTORY} \
+             ${PRODUCTION_RESOURCES_MEDIA_DIRECTORY} \
+             ${PRODUCTION_RESOURCES_CERTIFICATES_DIRECTORY}
+  "
 }
 
 
 # Main
 
-export SSHPASS=${PRODUCTION_SERVER_PASSWORD}
+alias run_on_production='sshpass -e ssh ${PRODUCTION_SERVER_USERNAME}@${PRODUCTION_SERVER_HOST}'
+shopt -s expand_aliases
 ssh-keyscan -t rsa ${PRODUCTION_SERVER_HOST} > ~/.ssh/known_hosts
-sshpass -e ssh ${PRODUCTION_SERVER_USERNAME}@${PRODUCTION_SERVER_HOST} "$(typeset -f); create_directory_structure"
+
+create_directory_structure_on_production
