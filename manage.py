4c53a61fee064deaa66bb76be157929a39a55da7
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2019-08-30 16:48:33 +0300

    Improve manage.py

--- manage.py
+++ manage.py
@@ -1,18 +1,24 @@
 import os
 import sys
 import gettext
+from time import sleep
 from argparse import ArgumentParser
 from functools import partial
 from subprocess import check_call
 
 _ = gettext.gettext
 
+REPOSITORY_URL = 'https://github.com/vsevolod-skripnik/vash.git'
+
 
 class Project:
     SERVICE_NAMES = ('bundler', 'engine', 'proxy')
-    
+
     def __init__(self, name):
+        print(_(f'Initializing project “{name}”...'))
         self.name = name
+
+        # Folders
 
         # ~/projects/vash/manage.py
         self.manage_file_path = os.path.abspath(__file__)
@@ -26,12 +32,12 @@ class Project:
         )
         # ~/projects/{name}
         self.project_folder_path = f'{self.projects_folder_path}/{name}'
-
+        
         # ~/projects/{name}/versions
         self.versions_folder_path = f'{self.project_folder_path}/versions'
-        # ~/projects/{name}/versions/rolling -> ~/projects/vash
-        self.rolling_version_path = f'{self.versions_folder_path}/rolling'
-
+        # ~/projects/{name}/versions/latest -> ~/projects/vash
+        self.latest_version_path = f'{self.versions_folder_path}/latest'
+        
         # ~/projects/{name}/resources
         self.resources_folder_path = f'{self.project_folder_path}/resources'
         # ~/projects/{name}/resources/{resource}
@@ -47,53 +53,24 @@ class Project:
         }
         # ~/projects/{name}/resources/pages/_html
         self.html_pages_folder_path = f'{self.resource_folder_paths["pages"]}/_html'
-
-        self.version: str
-        self.version_folder_path: str
         
-        self.environment: dict
-
-    def set_version(self, version):
-        self.version = version
-        self.version_folder_path = f'{self.versions_folder_path}/{version}'
-
-    def set_environment(self):
-        print(_('Creating environment...'))
-
-        environment_variables = {
-            'USER_ID': f'{os.getuid()}',
-            'PROJECT_FOLDER': f'{self.version_folder_path}',
-            'RESOURCES_FOLDER': f'{self.resources_folder_path}',
-        }
-
-        for service_name in self.SERVICE_NAMES:
-            variable_name = f'DOCKER_{service_name.upper()}_IMAGE'
-            variable_value = f'{self.name}_{service_name}:{self.version}'
-            environment_variables[variable_name] = variable_value
-
-        for resource_name, resource_folder_path in self.resource_folder_paths.items():
-            variable_name = f'{resource_name.upper()}_FOLDER'
-            environment_variables[variable_name] = f'{resource_folder_path}'
-        
-        self.environment = environment_variables 
-        for variable_name, variable_value in self.environment.items():
-            print(_(f'  “{variable_name}” = “{variable_value}”'))
-            os.environ[variable_name] = variable_value
-        print()
-
-    def initialize(self):
-        print(_(f'Initializing project “{self.name}”...'))
         self.create_folders()
-        self.create_rolling()
-        print()
-    
-    def run(self, version, as_daemon=False):
-        print(_(f'Running project “{self.name}”...'))
-        self.set_version(version)
-        self.set_environment()
-        self.run_project(as_daemon)
-        print()
-    
+
+        # Versions
+
+        self.version = 'latest'
+        self.version_folder_path = f'{self.versions_folder_path}/{self.version}'
+        if not os.path.islink(self.latest_version_path):
+            print(_(f'  “{self.latest_version_path}”'))
+            print(_(f'   -> “{self.repository_folder_path}”'))
+            os.symlink(self.repository_folder_path, self.latest_version_path, target_is_directory=True)
+        #
+        self.installed_versions = os.listdir(self.versions_folder_path)
+
+        # Environment
+
+        self.environment = self.get_environment()
+
     def create_folders(self):
         print(_('Creating folders...'))
 
@@ -114,19 +91,64 @@ class Project:
         else:
             print()
 
-    def create_rolling(self):
-        print(_('Creating rolling version folder...'))
-        if not os.path.islink(self.rolling_version_path):
-            print(_(f'  “{self.rolling_version_path}”'))
-            print(_(f'   -> “{self.repository_folder_path}”'))
-            os.symlink(self.repository_folder_path, self.rolling_version_path, target_is_directory=True)
-        else:
-            print(_('  Exists.'))
+    def install(self, version):
+        print(_(f'Installing version “{version}”'))
+
+        working_folder_before_install = os.getcwd()
+
+        version_folder_path = f'{self.versions_folder_path}/{version}'
+
+        os.chdir(self.versions_folder_path)
+        command = ['git', 'clone', f'{REPOSITORY_URL}', f'{version}']
+        check_call(command)
+
+        os.chdir(version_folder_path)
+        command = ['git', 'checkout', f'{version_folder_path}']
+        check_call(command)
+
+        command = ['git', 'reset', '--hard']
+        check_call(command)
+
+        os.chdir(working_folder_before_install)
+
+        self.installed_versions.append(version)
         print()
-    
-    def run_project(self, as_daemon=False):
-        print(_(f'Running project...'))
-    
+
+    def get_environment(self):
+        environment_variables = {
+            'USER_ID': f'{os.getuid()}',
+            'RESOURCES_FOLDER': f'{self.resources_folder_path}',
+        }
+
+        for service_name in self.SERVICE_NAMES:
+            variable_name = f'DOCKER_{service_name.upper()}_IMAGE'
+            variable_value = f'{self.name}_{service_name}:{self.version}'
+            environment_variables[variable_name] = variable_value
+
+        for resource_name, resource_folder_path in self.resource_folder_paths.items():
+            variable_name = f'{resource_name.upper()}_FOLDER'
+            environment_variables[variable_name] = f'{resource_folder_path}'
+
+        return environment_variables
+
+    def set_environment(self):
+        print('Setting environment...')
+        self.environment = self.get_environment()
+        for variable_name, variable_value in self.environment.items():
+            print(_(f'  “{variable_name}” = “{variable_value}”'))
+            os.environ[variable_name] = variable_value
+        print()
+
+    def run(self, version, as_daemon=False):
+        if version not in self.installed_versions:
+            self.install(version)
+
+        self.version = version
+        self.version_folder_path = f'{self.versions_folder_path}/{version}'
+
+        print(_(f'Running “{self.name}” version “{version}”...'))
+        self.set_environment()
+
         command = [
             'docker-compose',
                 '-f', f'{self.version_folder_path}/compose/engine.yml',
@@ -140,13 +162,15 @@ class Project:
         if as_daemon:
             command.append('--detach')
         check_call(command)
+
         print()
 
     def create_index_page(self):
         print(_('Creating index page...'))
-        
+
         if not os.path.isdir(self.html_pages_folder_path):
             os.makedirs(self.html_pages_folder_path)
+
         page_path = f'{self.html_pages_folder_path}/index.html'
         if not os.path.isfile(page_path):
             page_content = (
@@ -167,16 +191,50 @@ class Project:
         else:
             print(_('  Exists.'))
         print()
+    
+    def test_index_page(self):
+        print(_('Testing index page...'))
+        import requests
+        url = 'https://127.0.0.1/'
+        max_tries = 3
+        for _ in range(max_tries):
+            try:
+                response = requests.get(url, verify=False)
+            except requests.exceptions.ConnectionError:
+                print('  Fail. Sleep 5 seconds...')
+                sleep(5)
+            else:
+                if response.status_code == 200:
+                    message = 'Fine.'
+                    is_fine = True
+                    break
+                else:
+                    message = f'Oh no: {response.status_code}'
+                    is_fine = False
+                    break
+        else:
+            message = 'Max tries exceeded.'
+            is_fine = False
+
+        print(f'  {message}')
+        print()
+        return is_fine
 
 
 def initialize_project(name):
-    project = Project(name)
-    project.initialize()
+    Project(name)
 
 
-def run_project(name, version, as_daemon=False):
+def run_project(name, version, as_daemon):
     project = Project(name)
     project.run(version, as_daemon)
+
+
+def deploy_project(name, version):
+    project = Project(name)
+    project.run(version, as_daemon=True)
+    project.create_index_page()
+    print(project.test_index_page())
 
 
 def get_command(argv):
@@ -192,7 +250,9 @@ def get_command(argv):
     elif namespace.category == 'run':
         argument_parser = ArgumentParser()
         argument_parser.add_argument('name', help=_('Project name.'))
-        argument_parser.add_argument('version', help=_('Project version.'))
+        argument_parser.add_argument(
+            '-v', '--version', default='latest', help=_('Project version.')
+        )
         argument_parser.add_argument(
             '-d', '--daemon', dest='as_daemon', 
             action='store_true', help=_('Enter daemon mode after start.')
