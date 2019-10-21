08ba4b8e13efeb702b27ff889f286bd3c8ac1da8
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2019-10-21 12:39:33 +0300

    Uncomment `test_meta_file.py`

--- images/engine/code/engine/tests/models/files/test_meta_file.py
+++ images/engine/code/engine/tests/models/files/test_meta_file.py
@@ -1,6 +1,16 @@
-# from engine.models.files.meta_file import MetaFile
-#
-#
-# def test_has_no_default_keys(path):
-#     meta_file = MetaFile(path)
-#     assert not meta_file.DEFAULT_KEYS
+import pytest
+
+from engine.models.files.meta_file import MetaFile
+from engine.tests.conftest import TEST_ROOT_FOLDER
+
+
+@pytest.fixture
+def model(request, monkeypatch):
+    model = MetaFile
+    monkeypatch.setattr(model, 'ROOT_FOLDER', TEST_ROOT_FOLDER, raising=True)
+    return model
+
+
+def test_has_no_default_keys(model, path):
+    meta_file = model(path)
+    assert not meta_file.DEFAULT_KEYS
