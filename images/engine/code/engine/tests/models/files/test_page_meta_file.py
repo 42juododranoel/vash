62881270fc5d499328ceb9954b12315a68d3e983
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2019-10-21 12:40:41 +0300

    Uncomment `test_page_meta_file.py`

--- images/engine/code/engine/tests/models/files/test_page_meta_file.py
+++ images/engine/code/engine/tests/models/files/test_page_meta_file.py
@@ -1,9 +1,17 @@
-# import pytest
-#
-# from engine.models.files.page_meta_file import PageMetaFile
-#
-#
-# @pytest.mark.parametrize('key', ['slug', 'type', 'title'])
-# def test_key_in_default_keys(path, key):
-#     page_meta_file = PageMetaFile(path)
-#     assert key in page_meta_file.DEFAULT_KEYS
+import pytest
+
+from engine.models.files.page_meta_file import PageMetaFile
+from engine.tests.conftest import TEST_ROOT_FOLDER
+
+
+@pytest.fixture
+def model(request, monkeypatch):
+    model = PageMetaFile
+    monkeypatch.setattr(model, 'ROOT_FOLDER', TEST_ROOT_FOLDER, raising=True)
+    return model
+
+
+@pytest.mark.parametrize('key', ['slug', 'type', 'title'])
+def test_key_in_default_keys(model, path, key):
+    page_meta_file = model(path)
+    assert key in page_meta_file.DEFAULT_KEYS
