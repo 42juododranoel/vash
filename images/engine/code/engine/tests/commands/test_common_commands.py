172507f44c014a10a87b10436b8859c121e0e8af
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2019-10-21 20:14:55 +0300

    Improve broken CLI tests, still broken

--- images/engine/code/engine/tests/commands/test_common_commands.py
+++ images/engine/code/engine/tests/commands/test_common_commands.py
@@ -1,19 +1,35 @@
-import pytest
-from click.testing import CliRunner
-
-from engine.commands.page import create_page, delete_page
-from engine.commands.template import create_template, delete_template
-
-
+# import pytest
+# from click.testing import CliRunner
+#
+# from engine.commands.page import create_page, delete_page
+# from engine.commands.template import create_template, delete_template
+# from engine.models.folders.page import Page
+# from engine.models.folders.template import Template
+# from engine.tests.conftest import (
+#     TEST_ROOT_FOLDER,
+#     RELATIVE_PATH_A,
+# )
+#
+#
+# @pytest.fixture(params=[Page, Template])
+# def model(request, monkeypatch):
+#     model = request.param
+#     monkeypatch.setattr(
+#         model,
+#         'ROOT_FOLDER',
+#         f'{TEST_ROOT_FOLDER}{model.ROOT_FOLDER}',
+#         raising=True
+#     )
+#     return model
+#
+#
 # @pytest.mark.parametrize('command', [create_page, create_template])
-# def test_common_create_commands(command):
-#     cli_runner = CliRunner()
-#     command_result = cli_runner.invoke(command, RESOURCE_PATHS)
+# def test_common_create_commands(command, ):
+#     command_result = CliRunner().invoke(command, RELATIVE_PATH_A)
 #     assert command_result.exit_code == 0
 #
 #
 # @pytest.mark.parametrize('command', [delete_page, delete_template])
 # def test_common_delete_commands(command):
-#     cli_runner = CliRunner()
-#     command_result = cli_runner.invoke(command, RESOURCE_PATHS)
+#     command_result = CliRunner().invoke(command, RELATIVE_PATH_A)
 #     assert command_result.exit_code == 0
